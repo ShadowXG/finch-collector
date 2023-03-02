008fee74bb4c4d55ea1829b1c67baafc4481df16
Travis <travishersh0@gmail.com>
2023-03-02 15:43:36 -0500

    set up the index view page for finches

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+# temporary finches
+finches = [
+    {
+        'species': 'American Goldfinch', 
+        'description': 'The American goldfinch is a small North American bird in the finch family', 
+        'location': "North America"
+    },
+    {
+        'species': 'Brambling', 
+        'description': 'The brambling is a small passerine bird in the finch family', 
+        'location': "widespread"
+    },
+]
+
 # Create your views here.
 
 # Home view
@@ -9,3 +23,8 @@ def home(request):
 # About view
 def about(request):
   return render(request, 'about.html')
+
+# Finches index view
+def finches_index(request):
+  # We pass data to a template very much like we did in Express!
+  return render(request, 'finches/index.html', { 'finches': finches })
